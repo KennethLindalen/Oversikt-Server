96851b2878f96de3e53f7187add0aea3978a7caf
kennethlindalen <kennethlindalen@gmail.com>
2021-01-30 02:44:41 +0100

    Created GsonConverterConfig

--- src/main/java/dev/kennethlindalen/rkupongoversikt/Controller/Config/GsonConverterConfig.java
+++ src/main/java/dev/kennethlindalen/rkupongoversikt/Controller/Config/GsonConverterConfig.java
@@ -1,4 +1,4 @@
-package dev.kennethlindalen.rkupongoversikt.Config;
+package dev.kennethlindalen.rkupongoversikt.Controller.Config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
